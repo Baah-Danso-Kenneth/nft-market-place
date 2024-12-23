34f510532c97deedc93b650a107999f8cc7102d6
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-12-23 10:48:20 +0000

    Implement Nft.sol logic and interact it with it using hard hat

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,3 +1,5 @@
+const { ethers } = require("hardhat");
+
 async function main() {
 
   const [deployer] = await ethers.getSigners();
@@ -7,9 +9,12 @@ async function main() {
 
   // deploy contracts here:
   
-  
+  const NFT = await ethers.getContractFactory("NFT");
+  const nft = await NFT.deploy();
+
+  console.log("NFT contract address",nft.address)
   // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
-  saveFrontendFiles();
+  saveFrontendFiles(nft, "NFT");
 }
 
 function saveFrontendFiles(contract, name) {
