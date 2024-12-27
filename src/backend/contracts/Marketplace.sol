c07540d9508c21c42fea2115088adc52e706caa6
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-12-27 03:45:15 +0000

    Write a smart contract for the marketplace

--- src/backend/contracts/Marketplace.sol
+++ src/backend/contracts/Marketplace.sol
@@ -4,15 +4,57 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-
-contract Marketplace is ReentrancyGuard{
-
+contract Marketplace is ReentrancyGuard {
     address payable public immutable feeAccount;
     uint public immutable feePercent;
+    uint public itemCount;
 
-    constructor(uint _feePercent){
+    struct Item {
+        uint itemId;
+        IERC721 nft;
+        uint tokenId;
+        uint price;
+        address payable seller;
+        bool sold;
+    }
+
+    event Offerred(
+        uint itemId,
+        address indexed nft,
+        uint price,
+        uint tokenId,
+        address indexed seller
+    );
+
+    mapping(uint => Item) public items;
+
+    // Constructor
+    constructor(uint _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
     }
 
+    // Make Item Function
+    function makeItem(IERC721 _nft, uint _tokenId, uint _price) external nonReentrant {
+        require(_price > 0, "Price must be greater than zero"); // Corrected _price condition
+        itemCount++;
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
+
+        emit Offerred(
+            itemCount,
+             address(_nft),
+             _price,
+             _tokenId,
+             msg.sender
+             );
+    }
 }
