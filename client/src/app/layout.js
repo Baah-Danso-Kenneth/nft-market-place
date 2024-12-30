0f49a7c07c038765e64efdea7bfc49e85736f5fb
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-12-30 02:29:47 +0000

    Wrap entire application with contextApi for state management

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { WalletContextProvider } from "./context/wallet";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,9 +20,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <WalletContextProvider>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
+      </WalletContextProvider>
     </html>
   );
 }
