1c4cf0ff429b361333e33d461fc86a807d2ac16b
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2024-12-30 16:36:11 +0000

    Components for header

--- client/src/app/components/header/Header.jsx
+++ client/src/app/components/header/Header.jsx
@@ -1,6 +1,11 @@
+"use client"
+
 import { WalletContext } from '@/app/context/wallet'
 import { BrowserProvider } from 'ethers';
 import React, { useContext } from 'react'
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './Header.module.css'
 
 function Header() {
 
@@ -14,34 +19,73 @@ function Header() {
     } = useContext(WalletContext);
 
 
-    const connectWallet = async()=>{
-        if(!window.ethereum){
-            throw new Error("Metamask is not installed")
+    const connectWallet = async () => {
+        if (!window.ethereum) {
+          throw new Error("Metamask is not installed");
         }
+    
         try {
-           const provider = new BrowserProvider(window.ethereum);
-           const signer = await provider.getSigner(); 
-           setSigner(signer)
-           const accounts = await provider.send("eth_requestAccounts", []);
-           setIsConnected(true)
-           setUserAddress(accounts[0]);
-
-           const network = await provider.getNetwork();
-           const chainID = network.chainId;
-           const sepoliaNetworkId = "11155111"
-
-           if(chainID.toString !== sepoliaNetworkId){
-            alert("Please join with sepolia network")
+          const provider = new BrowserProvider(window.ethereum);
+          const signer = await provider.getSigner();
+          setSigner(signer);
+          const accounts = await provider.send("eth_requestAccounts", []);
+          setIsConnected(true);
+          setUserAddress(accounts[0]);
+          const network = await provider.getNetwork();
+          const chainID = network.chainId;
+          const sepoliaNetworkId ="11155111";
+    
+          if (chainID.toString() !== sepoliaNetworkId) {
+            alert("Please switch your MetaMask to sepolia network");
             return;
-           }
+          }
         } catch (error) {
-            console.log("connection error",error)
+          console.error("connection error: ", error);
         }
-    } 
+      };
 
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header className={styles.header}>
+          <div className={styles.container}>
+            <div className={styles.logo}>
+              <Link href="/">
+                <Image src="/logo.png" width={280} height={44} alt="logo" />
+              </Link>
+            </div>
+            <nav className={styles.nav}>
+              <ul className={styles.navLinks}>
+                <li>
+                  <Link href="/marketplace" className={styles.link}>
+                    MarketPlace
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/sellNFT" className={styles.link}>
+                    List
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/profile" className={styles.link}>
+                    Profile
+                  </Link>
+                </li>
+              </ul>
+              <button
+                className={`${styles.ctaBtn} ${
+                  isConnected ? styles.activebtn : styles.inactivebtn
+                }`}
+                onClick={connectWallet}
+              >
+                {isConnected ? (
+                  <>{userAddress?.slice(0, 8)}...</>
+                ) : (
+                  "Connect wallet"
+                )}
+              </button>
+            </nav>
+          </div>
+        </header>
+      );
 }
 
 export default Header
