cb28a88da45b930a52ed377edd7add25354780ad
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-07 21:41:20 +0000

    Restructure code

--- ignition/modules/Token.js
+++
@@ -1,8 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-const DeployModule = buildModule("TokenModule", (m)=>{
-  const marketPlace = m.contract("NFTSTORE");
-  return marketPlace;
-});
-
-module.exports = DeployModule;
