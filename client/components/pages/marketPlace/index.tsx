bbf408f23c105c1a674cb2891a3d3bdf94a5045a
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-10 23:47:15 +0000

    Place the main carousel on the marketplace page

--- client/components/pages/marketPlace/index.tsx
+++ client/components/pages/marketPlace/index.tsx
@@ -1,11 +1,13 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
+import CarouselWrapper from './CarouselWrapper'
 
 function MarketPlace() {
   return (
     <div>
         <Header/>
+        <CarouselWrapper/>
         <Footer/>
     </div>
   )
