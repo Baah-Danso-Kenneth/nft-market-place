4194e9bda7aec2bc57787a946d8ad4b9db7ac3f9
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-12 08:00:47 +0000

    Logically create details page with neccessary content

--- client/components/pages/detailsNft/DetailContent.tsx
+++ client/components/pages/detailsNft/DetailContent.tsx
@@ -7,6 +7,8 @@ import GetIpfsUrlFromPinata from "@/app/utils";
 import Image from "next/image";
 import { WalletContext } from "@/content/wallet";
 import MarketPlaceJson from "../../../app/marketplace.json";
+import { Book, Brush, Clock, Eye, Heart, Menu, Send, ShoppingCart } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 type Item = {
   price: string;
@@ -99,23 +101,147 @@ function DetailContent() {
       {isConnected ? (
         <>
           {item ? (
-            <>
-              <div>
-                <Image
-                  src={item.image}
-                  alt={item.name}
-                  width={300}
-                  height={300}
-                  className="object-cover"
-                />
-                <h2>{item.name}</h2>
-                <p>{item.description}</p>
-                <p>Price: {item.price} ETH</p>
-                <p>Seller: {item.seller}</p>
-                <button onClick={buyNFT}>{btnContent}</button>
-                {msg && <p>{msg}</p>}
+            <div className="mx-10 py-10 flex gap-10">
+
+        <div className='w-[40%]'>
+            <div className="h-14 rounded-tl-2xl rounded-tr-2xl bg-[#202020] w-full" />
+              <Image
+              src={item.image}
+              alt={item.name}
+              width={1080}
+              height={250}
+              className='w-full h-[80vh] object-cover rounded-bl-2xl rounded-br-2xl'
+            />
+          </div>
+
+
+          <div className='w-[60%]'>
+            <div className='text-start font-nunito text-[15px] text-nowrap'>
+              <h1>{item.name}</h1>
+            </div>
+
+            <div className='text-start font-nunito text-[2rem] font-extrabold text-nowrap'>
+              <h1>{item.name}</h1>
+            </div>
+
+            <div className='text-start font-nunito text-[15px] text-nowrap'>
+              <p className='text-[1.8rem]'>Owned by <span className='text-[16px] '>{item.owner}</span></p>
+            </div>
+
+            <div className="flex space-x-5 items-center mb-7" >
+              <div className='flex space-x-3'>
+                <Eye/>
+                <h1>1.2k view</h1>
               </div>
-            </>
+              <div className='flex space-x-3'>
+                <Heart/>
+                <h1>Likes</h1>
+              </div>
+              <div className='flex space-x-3'>
+                <Brush/>
+                <h1>view</h1>
+              </div>
+            </div>
+
+            <div className='w-full rounded-2xl h-[30vh] border border-[#202020]'>
+
+              <div className='flex space-x-5 py-3 border-b border-[#202020] font-nunito'>
+                <Clock className='ml-3'/>
+                <p>Sales ends june 21, 2025 at 1:06pm</p>
+              </div>
+
+              <div className='ml-3 mt-2 font-nunito font-bold'>
+                <h1>Current price</h1>
+
+                <div className='flex space-x-3 items-center'>
+                  <div>
+                    <h1 className='text-[2rem]'>{item.price}ETH</h1>
+                  </div>
+                  <div>
+                  <p>$362.00</p>
+                  </div>
+                </div>
+              </div>
+
+
+              <div className='flex gap-10'>
+
+              <div className='w-[50%] h-auto flex ml-5'>
+  <Button 
+    onClick={buyNFT} 
+    className="w-full bg-blue-700 rounded-md flex justify-between items-center"
+  >
+    <div className="w-[80%] mx-3 border-r border-white ">
+      <h1 className="text-center font-nunito text-white uppercase text-2xl">Buy Nft</h1>
+    </div>
+    <div className="py-3">
+      <ShoppingCart className="text-white" />
+    </div>
+  </Button>
+</div>;
+
+                
+<div className='w-[50%] h-auto flex ml-5'>
+  <Button 
+    onClick={buyNFT} 
+    className="w-full bg-red-700 rounded-md flex justify-between items-center"
+  >
+    <div className="w-[80%] mx-3 border-r border-white">
+      <h1 className="text-center font-nunito text-white uppercase text-2xl">cancel</h1>
+    </div>
+    <div className="py-3">
+      <ShoppingCart className="text-white" />
+    </div>
+  </Button>
+</div>;
+
+              </div>
+
+
+            </div>
+
+            <div className='w-full rounded-2xl h-auto border mt-7 border-[#202020]'>
+
+             <div className='flex  space-x-3 border-b border-[#202020] py-2 font-nunito'>
+              <Menu className='ml-3'/>
+              <h1>Description</h1>
+             </div>
+
+             <div className='mt-2 font-nunito border-b border-[#202020] '>
+               <h1 className='ml-3 text-[20px]'>By <span className='font-bold'>lilyillo</span></h1>
+               <p className='mx-3 mb-2'>{item.description}</p>
+             </div>
+
+             <div className='flex  space-x-3 border-b border-[#202020] py-2 font-nunito'>
+              <Book className='ml-3'/>
+              <h1>Details</h1>
+             </div>
+
+             <div className='flex justify-between mx-3 font-nunito text-[18px] mt-3  mb-3'>
+              <div className='space-y-3'>
+                <h1>Contract Address</h1>
+                <h1>Token ID</h1>
+                <h1>Token Standard</h1>
+                <h1>Metadata</h1>
+                <h1>Creator Earnings</h1>
+              </div>
+
+              <div className='space-y-3  flex flex-col justify-end'>
+              <h1>0x498a........</h1>
+              <h1>{item.tokenId}</h1>
+              <h1>ERC-7200</h1>
+              <h1>Frozen</h1>
+              <h1>10%</h1>
+              </div>
+
+             </div>
+
+
+            </div>
+
+          </div>
+              
+            </div>
           ) : (
             <p>Loading item details...</p>
           )}
